f89eaac839a4aced7f7606db32f258f076cc812b
zhangfengqi937 <zhangfengqi937@gmail.com>
2021-01-05 17:48:51 -0500

    Enforce model validation in list view

--- tombApp/tests/test_views.py
+++ tombApp/tests/test_views.py
@@ -113,6 +113,17 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
+    
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 class NewListTest(TestCase):
 
@@ -167,3 +178,4 @@ class NewItemTest(TestCase):
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
+    
