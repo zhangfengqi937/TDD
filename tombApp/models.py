027f8cfc67f6690b2797dd3edcf2bec0b1d0cbad
zhangfengqi937 <zhangfengqi937@gmail.com>
2021-01-05 17:55:04 -0500

    Use get_abolute_url on List model to dry urls in views. END of Ch.13

--- tombApp/models.py
+++ tombApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Item(object):
@@ -8,7 +9,8 @@ class Item(models.Model):
     text = models.TextField()
 
 class List(models.Model):
-    pass
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
     text = models.TextField(default='')
--- tombApp/tests/test_models.py
+++ tombApp/tests/test_models.py
@@ -114,3 +114,7 @@ class NewItemTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+     def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
