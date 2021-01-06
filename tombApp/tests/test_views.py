fdee047f6ed17d484264bfab14dc75db8572aa1d
zhangfengqi937 <zhangfengqi937@gmail.com>
2021-01-05 19:45:23 -0500

    End of Ch.15

--- tombApp/tests/test_views.py
+++ tombApp/tests/test_views.py
@@ -6,9 +6,13 @@ from django.utils.html import escape
 
 from tombApp.views import home_page 
 from tombApp.models import Item, List
-from tombApp.forms import ItemForm, EMPTY_ITEM_ERROR
 
 
+from tombApp.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
+from unittest import skip
 import unittest
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -99,8 +103,8 @@ class ListViewTest(TestCase):
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
-        response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def test_displays_only_items_for_that_list(self):
@@ -154,12 +158,26 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
-        
+
+    
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
